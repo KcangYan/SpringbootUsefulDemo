4d4743871c5ca941eb39c4168470004f70aa898e
KcangYan <KcangYan@users.noreply.github.com>
2020-01-07 11:11:47 +0800

    Create DruidConfig.java

---
+++ src/main/java/com/kcangyan/usefulDemo/config/DruidConfig.java
@@ -0,0 +1,34 @@
+package com.kcangyan.usefulDemo.config;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DruidConfig {
+    //@Value("${zidingyi.DruidAdminIP}")
+    private String ip = MyselfConfig.getDruidConfigIP();
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),
+                "/druid/*");
+        servletRegistrationBean.addInitParameter("allow",ip);
+        //登录查看信息的账号密码.
+        servletRegistrationBean.addInitParameter("loginUsername","admin");
+        servletRegistrationBean.addInitParameter("loginPassword","123456");
+        //是否能够重置数据
+        servletRegistrationBean.addInitParameter("resetEnable","false");
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean druidStatFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+}
